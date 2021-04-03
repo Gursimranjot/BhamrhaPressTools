5b6cd4ffd450a1f0488a5a7353f9031921809343
gursimran <simran23dhiman@gmail.com>
2021-04-03 23:19:43 +0530

    gallery added  and inner page

--- js/common.js
+++ js/common.js
@@ -168,6 +168,29 @@ $('.counter').each(function () {
 //     });
 // };
 
+
+$('.image-popup-vertical-fit').magnificPopup({
+	type: 'image',
+  mainClass: 'mfp-with-zoom', 
+  gallery:{
+			enabled:true
+		},
+
+  zoom: {
+    enabled: true, 
+
+    duration: 300, // duration of the effect, in milliseconds
+    easing: 'ease-in-out', // CSS transition easing function
+
+    opener: function(openerElement) {
+
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+  }
+}
+
+});
+
+
 /* ===============================================
     5. YOUTUBE POPUP
 =============================================== */
